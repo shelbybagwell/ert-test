de5c4eba6ada3bbc9fccca41a4ee6504df1d8801
Shelby Bagwell <sbagwel2@vols.utk.edu>
2025-06-12 21:13:38 -0500

    massive code clean up

--- Mandelbrot/index.js
+++ Mandelbrot/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const {Command} = require('commander');
-const {math} = require('mathjs');
+const math = require('mathjs');
 
 const program = new Command();
 
@@ -10,71 +10,72 @@ program
     .description('solves the mandelbrot equation')
     .version('1.0.0');
 
-// Define program options
+// Define program action and options
 program
-    .option('-x, --center-x', 
-        'x-coordinate of the center of the view')
-    .option('-y, --center-y', 
-        'y-coordinate of the center of the view')
-    .option('-w, --width', 
-        'width of the view')
-    .option('-h, --height', 
-        'height of the view')
-    .option('-s, --step', 
-        'resolution of the view (size of a pixel)')
-    .option('-n, --max-iterations', 
-        'maximum number of iterations to perform at each pixel', 
+    .command('mandelbrot')
+    .argument('-x, --center-x', 
+        'x-coordinate of the center of the view', parseFloat)
+    .argument('-y, --center-y', 
+        'y-coordinate of the center of the view', parseFloat)
+    .argument('-w, --width', 
+        'width of the view', parseFloat)
+    .argument('-h, --height', 
+        'height of the view', parseFloat)
+    .argument('-s, --step', 
+        'resolution of the view (size of a pixel)', parseFloat)
+    .argument('-n, --max-iterations', 
+        'maximum number of iterations to perform at each pixel', parseInt, 
         100)
-    .option('-b, --bound', 
-        'divergence cutoff. If the abs(z) >= bound, the iterations stop.', 
+    .argument('-b, --bound', 
+        'divergence cutoff. If the abs(z) >= bound, the iterations stop.', parseFloat, 
         2)
-    .option('-p, --power', 
-        'exponent used in the mandelbrot equation', 
-        2);
+    .argument('-p, --power', 
+        'exponent used in the mandelbrot equation', parseFloat, 
+        2)
+    .action((centerX, centerY, width, height, step, maxIterations, bound, power, options) => {
+        let minX = centerX - width;
+        let maxX = centerX + width;
+        let minY = centerY - height;
+        let maxY = centerY + height;
 
-// Define program actions
-program.action((options) => {
-    let minX = options.center-x - options.width;
-    let maxX = options.center-x + options.width;
-    let minY = options.center-y - options.height;
-    let maxY = options.center-y + options.height;
+        // Create evenly spaced array
+        let x_domain = linspace(minX, maxX, step);
+        let y_domain = linspace(minY, maxY, step);
 
-    // Create evenly spaced array
-    let x_domain = linspace(minX, maxX, options.step);
-    let y_domain = linspace(minY, maxY, options.step);
+        let iterArr = [];
 
-    let iterArr = [];
-
-    for (let i=0; i<y_domain.length; i++) {
-        let row = [];
-        for (let j=0; j<x_domain.length; j++) {
-            let z = 0;
-            let p = options.power;
-            let c = math.complex(x_domain[j], y_domain[i]);
-            let invalid = false;
-            for (let k=0; k<options.max-iterations; k++) {
-                if (Math.abs(z) >= options.bound) {
-                    row.push(k);
-                    invalid = true;
-                    break;
-                } else {
-                    try {
-                        z = z**p + c;
-                        row.push(0);
-                    } catch (error) {
-                        console.log("An error occurred", error)
+        for (let y of y_domain) {
+            let row = [];
+            for (x of x_domain) {
+                let z = math.complex(0,0);
+                let p = options.power;
+                let c = math.complex(x, y);
+                let invalid = false;
+                for (let iter=0; iter<maxIterations; iter++) {
+                    if (Math.abs(z) >= bound) {
+                        row.push(iter);
+                        invalid = true;
+                        break;
+                    } else {
+                        try {
+                            z = math.add(math.pow(z, p), c);
+                        } catch (error) {
+                            console.log("An error occurred", error)
+                        }
                     }
                 }
+                if (!invalid) {
+                    row.push(0);
+                }
             }
-            if (!invalid) {
-                row.push(0);
-            }
+            iterArr.push(row);
         }
-        iterArr.push(row);
-    }
 
+        // Output the data
+        console.log(iterArr);
+    })
 
-})
+program.parse(process.argv);
 
 /*
 Creates an evenly spaced array given a starting value,
@@ -86,4 +87,5 @@ function linspace(start, stop, count) {
     for (let i = 0; i < count; i++) {
         arr.push(start + (step*i));
     }
+    return arr;
 }
