c8859eed62bf690e55ea437338a5f4cb2f5a1063
Shelby Bagwell <sbagwel2@vols.utk.edu>
2025-06-12 20:46:46 -0500

    calculate mandelbrot

--- Mandelbrot/index.js
+++ Mandelbrot/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const {Command} = require('commander');
+const {math} = require('mathjs');
 
 const program = new Command();
 
@@ -37,4 +38,52 @@ program.action((options) => {
     let maxX = options.center-x + options.width;
     let minY = options.center-y - options.height;
     let maxY = options.center-y + options.height;
+
+    // Create evenly spaced array
+    let x_domain = linspace(minX, maxX, options.step);
+    let y_domain = linspace(minY, maxY, options.step);
+
+    let iterArr = [];
+
+    for (let i=0; i<y_domain.length; i++) {
+        let row = [];
+        for (let j=0; j<x_domain.length; j++) {
+            let z = 0;
+            let p = options.power;
+            let c = math.complex(x_domain[j], y_domain[i]);
+            let invalid = false;
+            for (let k=0; k<options.max-iterations; k++) {
+                if (Math.abs(z) >= options.bound) {
+                    row.push(k);
+                    invalid = true;
+                    break;
+                } else {
+                    try {
+                        z = z**p + c;
+                        row.push(0);
+                    } catch (error) {
+                        console.log("An error occurred", error)
+                    }
+                }
+            }
+            if (!invalid) {
+                row.push(0);
+            }
+        }
+        iterArr.push(row);
+    }
+
+
 })
+
+/*
+Creates an evenly spaced array given a starting value,
+stopping value, and the number of elements desired.
+*/
+function linspace(start, stop, count) {
+    const step = (stop - start) / (count - 1);
+    let arr = [];
+    for (let i = 0; i < count; i++) {
+        arr.push(start + (step*i));
+    }
+}
